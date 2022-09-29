6d862135eaa243e64b40a12c4651ffdca8859dc8
kneerace <kneerace@gmail.com>
2022-09-29 10:52:05 -0400

    deleting backend folder

--- MoviesTheaterShows_BackEnd/src/errors/errorHandler.js
+++
@@ -1,7 +0,0 @@
-function errorhandler(error, req, res, next){
-    const {status= 500, 
-        message= `Something went wrong`} = error;
-        res.status(status).json({error: message});
-}
-
-module.exports = errorhandler;
--- MoviesTheaterShows_BackEnd/src/errors/methodNotAllowed.js
+++
@@ -1,8 +0,0 @@
-function methodnotAllowed(req, res, next){
-    next({
-        status: 405, 
-        message: `${req.method} not allowerd for ${req.originalUrl}`,
-    });
-}
-
-module.exports = methodnotAllowed;
